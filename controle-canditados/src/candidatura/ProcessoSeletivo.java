6a16a20b93bcdda2e07fe47e5e3c2ec898579731
Kaikeferreirax <kaikeferreira106@gmail.com>
2024-04-06 17:58:20 -0300

    Criando projeto

---
+++ controle-canditados/src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,18 @@
+package candidatura;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+
+            sc.close();
+
+        }
+}
