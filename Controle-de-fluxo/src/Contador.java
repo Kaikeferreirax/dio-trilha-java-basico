42b0fb4a8a3f7940be88fc29edf7d930f3a95e1e
Kaikeferreirax <kaikeferreira106@gmail.com>
2024-04-09 21:25:26 -0300

    Criando projeto POO

--- Controle-de-fluxo/src/Contador.java
+++ Controle-de-fluxo/src/Contador.java
@@ -23,17 +23,17 @@ public class Contador {
 
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
         //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-        if (parametroUm > parametroDois){
+        if (parametroUm > parametroDois) {
             throw new ParametrosInvalidosException();
         }
 
         int contagem = parametroDois - parametroUm;
         //realizar o for para imprimir os números com base na variável contagem
-        for(int i = 1; i <= contagem;i++){
+        for (int i = 1; i <= contagem; i++) {
             System.out.println("IMPRIMINDO " + i);
 
 
-    }
+        }
 
     }
 }
