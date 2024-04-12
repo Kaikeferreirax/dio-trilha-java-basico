e4d0345c6a59af8ae5cdec8f8287f85ed398c444
Kaikeferreirax <kaikeferreira106@gmail.com>
2024-04-12 18:24:16 -0300

    Implementando código

--- Pilhas/src/one/digitalinnovation/Main.java
+++ Pilhas/src/one/digitalinnovation/Main.java
@@ -3,5 +3,20 @@ package one.digitalinnovation;
 public class Main {
     public static void main(String[] args) {
 
+    // criando projeto
+    Pilha minhaPilha = new Pilha();
+
+    // acrescentando parâmetros
+    minhaPilha.push(new No(1));
+    minhaPilha.push(new No(2));
+    minhaPilha.push(new No(3));
+    minhaPilha.push(new No(4));
+    minhaPilha.push(new No(5));
+    minhaPilha.push(new No(6));
+
+    // removendo parâmetros
+    minhaPilha.pop();
+        System.out.println(minhaPilha);
+        System.out.println(minhaPilha.top());
     }
 }
