2ac7fb6eac13381d8831a264cd85e12f15af22ac
Kaikeferreirax <kaikeferreira106@gmail.com>
2024-04-17 17:48:15 -0300

    Implementando remove

--- ListaDuplamenteEncadeada/src/one/digitalinnovation/ListaDuplamenteEncadeada.java
+++ ListaDuplamenteEncadeada/src/one/digitalinnovation/ListaDuplamenteEncadeada.java
@@ -54,6 +54,23 @@ public class ListaDuplamenteEncadeada<T> {
         tamanhoLista++;
     }
 
+    public void remove(int index){
+        if(index == 0){
+            primeiroNo = primeiroNo.getNoProximo();
+            if(primeiroNo != null){
+                primeiroNo.setNoPrevio(null);
+            }
+        }else {
+            NoDuplo<T> noAuxiliar = getNo(index);
+            noAuxiliar.getNoPrevio().setNoProximo(noAuxiliar.getNoProximo());
+            if(noAuxiliar != ultimoNo){
+                noAuxiliar.getNoProximo().setNoPrevio(noAuxiliar.getNoPrevio());
+            }else{
+                ultimoNo = noAuxiliar;
+            }
+        }
+        this.tamanhoLista--;
+    }
 
 
     private NoDuplo<T> getNo(int index){
