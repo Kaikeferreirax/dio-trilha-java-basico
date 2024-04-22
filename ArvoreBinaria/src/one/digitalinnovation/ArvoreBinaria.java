a40378a4ff0e36c35e704fd74b5505c878d04424
Kaikeferreirax <kaikeferreira106@gmail.com>
2024-04-21 22:45:24 -0300

    Implementando ArvoreBinaria

---
+++ ArvoreBinaria/src/one/digitalinnovation/ArvoreBinaria.java
@@ -0,0 +1,30 @@
+package one.digitalinnovation;
+
+public class ArvoreBinaria<T extends  Comparable <T>> {
+
+
+    private BinNo<T> raiz;
+
+
+    public ArvoreBinaria(){
+    this.raiz = null;
+    }
+
+    public void inserir(T conteudo){
+        BinNo<T> novoNo = new BinNo<>(conteudo);
+        raiz = inserir(raiz,novoNo);
+    }
+
+    private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novoNo){
+        if(atual == null){
+            return novoNo;
+        } else if (novoNo.getConteudo().compareTo(atual.getConteudo()) < 0) {
+            atual.setNoEsq(inserir(atual.getNoEsq(), novoNo));
+        }else {
+            atual.setNoDir(inserir(atual.getNoDir(), novoNo));
+        }
+        return atual;
+    }
+
+
+}
