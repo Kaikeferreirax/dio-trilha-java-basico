427ad0cf2406a6ab1412def3b8cfa36f6192ee58
Kaikeferreirax <kaikeferreira106@gmail.com>
2024-04-22 00:33:28 -0300

    Implementando  primeira parte remove ArvoreBinaria

--- ArvoreBinaria/src/one/digitalinnovation/ArvoreBinaria.java
+++ ArvoreBinaria/src/one/digitalinnovation/ArvoreBinaria.java
@@ -68,4 +68,33 @@ public class ArvoreBinaria<T extends  Comparable <T>> {
         }
     }
 
+    public void remover(T conteudo){
+        try {
+            BinNo<T> atual = this.raiz;
+            BinNo<T> pai = null;
+            BinNo<T> filho = null;
+            BinNo<T> temp = null;
+
+            while(atual != null && !atual.getConteudo().equals(conteudo)){
+                pai = atual;
+                if(conteudo.compareTo(atual.getConteudo()) < 0){
+                    atual = atual.getNoEsq();
+                }else {
+                    atual = atual.getNoDir();
+
+                }
+
+                if(atual == null){
+                    System.out.println("Conteúdo não encontrado. Bloco try");
+
+                }
+
+            }
+
+        }
+        catch(NullPointerException erro){
+            System.out.println("Conteúdo não encontrado. Bloco Catch");
+        }
+    }
+
 }
