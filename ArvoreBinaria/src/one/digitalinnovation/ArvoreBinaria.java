2863af2c2095c506f19c3cc9a541ab2592b531b4
Kaikeferreirax <kaikeferreira106@gmail.com>
2024-04-22 15:37:17 -0300

    Implementando  segunda parte remove ArvoreBinaria

--- ArvoreBinaria/src/one/digitalinnovation/ArvoreBinaria.java
+++ ArvoreBinaria/src/one/digitalinnovation/ArvoreBinaria.java
@@ -88,6 +88,34 @@ public class ArvoreBinaria<T extends  Comparable <T>> {
                     System.out.println("Conteúdo não encontrado. Bloco try");
 
                 }
+                if(pai == null){
+                    if(atual.getNoDir() == null){
+                        this.raiz = atual.getNoEsq();
+                    }else if(atual.getNoEsq() == null){
+                        this.raiz = atual.getNoDir();
+
+                    }else{
+                        for(temp = atual, filho = atual.getNoEsq();
+                        filho.getNoDir() != null;
+                        temp = filho, filho = filho.getNoEsq()
+                        ){
+                            if(filho != atual.getNoEsq()){
+                                temp.setNoDir(filho.getNoEsq());
+                                filho.setNoEsq(raiz.getNoEsq());
+                            }
+
+                        }
+                        filho.setNoDir(raiz.getNoDir());
+                        raiz = filho;
+                    }
+
+                }else if(atual.getNoDir() == null) {
+
+                }else if(atual.getNoEsq() == null){
+
+                }else{
+
+                }
 
             }
 
