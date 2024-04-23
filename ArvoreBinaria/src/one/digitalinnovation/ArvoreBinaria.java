f500de1ad0ba59e0dbaf90cce7246d6cea86fca6
Kaikeferreirax <kaikeferreira106@gmail.com>
2024-04-23 01:36:02 -0300

    Implementando terceira parte remove ArvoreBinaria

--- ArvoreBinaria/src/one/digitalinnovation/ArvoreBinaria.java
+++ ArvoreBinaria/src/one/digitalinnovation/ArvoreBinaria.java
@@ -110,11 +110,34 @@ public class ArvoreBinaria<T extends  Comparable <T>> {
                     }
 
                 }else if(atual.getNoDir() == null) {
-
+                    if(pai.getNoEsq() == atual){
+                        pai.setNoEsq(atual.getNoEsq());
+                    } else {
+                        pai.setNoDir(atual.getNoEsq());
+                    }
                 }else if(atual.getNoEsq() == null){
-
+                    if(pai.getNoEsq() == atual){
+                        pai.setNoEsq(atual.getNoDir());
+                    } else {
+                        pai.setNoDir(atual.getNoDir());
+                    }
                 }else{
-
+                    for(
+                            temp = atual, filho = atual.getNoEsq();
+                            filho.getNoDir() != null;
+                            temp = filho, filho = filho.getNoDir()
+                    ){
+                        if(filho != atual.getNoEsq()){
+                            temp.setNoDir(filho.getNoEsq());
+                            filho.setNoEsq(atual.getNoEsq());
+                        }
+                        filho.setNoDir(atual.getNoDir());
+                        if(pai.getNoEsq() == atual){
+                            pai.setNoEsq(filho);
+                        } else {
+                            pai.setNoDir(filho);
+                        }
+                    }
                 }
 
             }
